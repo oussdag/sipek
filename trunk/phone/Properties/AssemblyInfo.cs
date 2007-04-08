8d6ff02f690e52f45f461b27c9d1ff56f6f627c1
sasacoh <sasacoh@21caa790-5027-0410-b7d3-b500383ceb39>
2007-04-08 21:10:13 +0000

    Make pjsipDll.dll as project reference Add windows installer "setup" project

--- trunk/phone/Properties/AssemblyInfo.cs
+++ trunk/phone/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SIPek")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SIP phone")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("sasacoh")]
-[assembly: AssemblyProduct("Gui")]
+[assembly: AssemblyProduct("SIPek")]
 [assembly: AssemblyCopyright("sasacoh")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
