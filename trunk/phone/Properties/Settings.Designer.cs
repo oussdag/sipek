c29eee8c97f2e7af397da33ff7e62b5f928b3944
sasacoh <sasacoh@21caa790-5027-0410-b7d3-b500383ceb39>
2007-05-09 20:39:35 +0000

    Default config corrections

--- trunk/phone/Properties/Settings.Designer.cs
+++ trunk/phone/Properties/Settings.Designer.cs
@@ -156,9 +156,14 @@ namespace Sipek.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>1341</string>\r\n  <string />\r\n  <string />\r\n  <string />\r\n</ArrayOfString>")]
+        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
+<ArrayOfString xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <string>1341</string>
+  <string>0</string>
+  <string>0</string>
+  <string>0</string>
+  <string>0</string>
+</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection cfgSipAccountIds {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["cfgSipAccountIds"]));
