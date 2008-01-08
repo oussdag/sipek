b5c9723295ea78b3d7fac730c9c200fd783ea5ec
sasacoh <sasacoh@21caa790-5027-0410-b7d3-b500383ceb39>
2008-01-08 10:11:13 +0000

    Minor changes

--- trunk/UnitTest/testTelephony.cs
+++ trunk/UnitTest/testTelephony.cs
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2007 Sasa Coh <sasacoh@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -30,7 +48,7 @@ namespace UnitTest
     //bool serviceRequest(EServiceCodes code, int session);
     public bool serviceRequest(int sessionId, int code, string dest) { return true; }
 
-    public bool dialDtmf(int sessionId,int mode, string digits) { return true; }
+    public bool dialDtmf(int sessionId, string digits, int mode) { return true; }
   }
 
   public class MockCommonProxy : CCommonProxyInterface
@@ -153,7 +171,7 @@ namespace UnitTest
       return true;
     }
 
-    public bool dialDtmf(int sessionId, int mode, string digits)
+    public bool dialDtmf(int sessionId, string digits, int mode)
     {
       //throw new Exception("The method or operation is not implemented.");
       return true;
