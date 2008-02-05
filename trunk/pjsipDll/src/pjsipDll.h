bb1b57471195ec52a47393430f081b26f46dd3ba
sasacoh <sasacoh@21caa790-5027-0410-b7d3-b500383ceb39>
2008-02-05 22:15:56 +0000

    Codec support added. Interface extended: extern "C" PJSIPDLL_DLL_API int dll_getNumOfCodecs(); extern "C" PJSIPDLL_DLL_API char* dll_getCodec(int index); extern "C" PJSIPDLL_DLL_API int dll_setCodecPriority(char* name, int index);

--- trunk/pjsipDll/src/pjsipDll.h
+++ trunk/pjsipDll/src/pjsipDll.h
@@ -50,6 +50,9 @@ extern "C" PJSIPDLL_DLL_API int onBuddyStatusChangedCallback(fptr_buddystatus cb
 extern "C" PJSIPDLL_DLL_API int dll_init(int listenPort);
 extern "C" PJSIPDLL_DLL_API int dll_shutdown(); 
 extern "C" PJSIPDLL_DLL_API int dll_main(void);
+extern "C" PJSIPDLL_DLL_API int dll_getNumOfCodecs();
+extern "C" PJSIPDLL_DLL_API char* dll_getCodec(int index);
+extern "C" PJSIPDLL_DLL_API int dll_setCodecPriority(char* name, int index);
 // pjsip call API
 extern "C" PJSIPDLL_DLL_API int dll_registerAccount(char* uri, char* reguri, char* name, char* username, char* password);
 extern "C" PJSIPDLL_DLL_API int dll_makeCall(int accountId, char* uri); 
