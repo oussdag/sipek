5b2ae85e02fba9da7f2d10be333de63913275a73
sasacoh <sasacoh@21caa790-5027-0410-b7d3-b500383ceb39>
2008-03-06 13:15:41 +0000

    Redesign commit: - namespace rename: Telephony -> CallControl - interfaces cleanup - removed functionality that are not relevant for call control - onConnect handler in Connecting state

--- trunk/telephony/callInterface.cs
+++ trunk/telephony/callInterface.cs
@@ -19,7 +19,8 @@
 
 using System;
 using System.Collections.Generic;
-namespace Telephony
+
+namespace Common
 {
   #region Enums
 
@@ -44,6 +45,22 @@ namespace Telephony
     EToneRing,
   }
 
+
+  public enum EServiceCodes : int
+  {
+    SC_CD,
+    SC_CFU,
+    SC_CFNR,
+    SC_DND,
+    SC_3PTY
+  }
+
+
+  public enum ECallNotification : int
+  { 
+    CN_HOLDCONFIRM
+  }
+ 
   #endregion
 
   public delegate void TimerExpiredCallback(object sender, EventArgs e);
@@ -60,7 +77,7 @@ namespace Telephony
   }
 
   /// <summary>
-  /// AbstractFactory is an abstract interace providing interfaces for Telephony module
+  /// AbstractFactory is an abstract interace providing interfaces for CallControl module
   /// It consists of two parts: factory methods and getter methods. First creating instances, 
   /// later returns instances. 
   /// </summary>
@@ -129,21 +146,100 @@ namespace Telephony
     #endregion Methods
   }
 
-  public interface ICommonProxyInterface
+
+  public delegate void DCallStateChanged(int callId, int callState, string info);
+  public delegate void DCallIncoming(int callId, string number, string info);
+  public delegate void DAccountStateChanged(int accountId, int accState);
+  public delegate void DMessageReceived(string from, string text);
+  public delegate void DBuddyStatusChanged(int buddyId, int status, string text);
+  public delegate void DDtmfDigitReceived(int callId, int digit);
+  public delegate void DCallNotification(int callId, ECallNotification notFlag, string text);
+  public delegate void DMessageWaitingNotification(int mwi, string text);
+
+
+  public abstract class ICommonProxyInterface
   {
-    int initialize(); 
-    int shutdown();
+    #region Events
+    /// <summary>
+    /// Events exposed to user layers. A protected virtual method added because 
+    /// derived classes cannot invoke events directly.
+    /// </summary>
+    /// 
 
-    int registerAccounts();
-    int registerAccounts(bool renew);
+    public event DCallStateChanged CallStateChanged;
+    protected void BaseCallStateChanged(int callId, int callState, string info)
+    {
+      if (null != CallStateChanged) CallStateChanged(callId, callState, info);
+    }
 
-    int addBuddy(string ident);
+    public event DCallIncoming CallIncoming;
+    protected void BaseIncomingCall(int callId, string number, string info)
+    {
+      if (null != CallIncoming) CallIncoming(callId, number, info);
+    }
 
-    int delBuddy(int buddyId);
+    public event DCallNotification CallNotification;
+    protected void BaseCallNotification(int callId, ECallNotification notifFlag, string text)
+    {
+      if (null != CallNotification) CallNotification(callId, notifFlag, text);
+    }
 
-    int sendMessage(string dest, string message);
+    public event DAccountStateChanged AccountStateChanged;
+    protected void BaseAccountStateChanged(int accountId, int accState)    
+    {
+      if (null != AccountStateChanged) AccountStateChanged(accountId, accState);
+    }
 
-    int setStatus(int accId, EUserStatus presence_state);
+    public event DMessageReceived MessageReceived;
+    protected void BaseMessageReceived(string from, string text)
+    {
+      if (null != MessageReceived) MessageReceived(from, text);
+    }
+
+    public event DBuddyStatusChanged BuddyStatusChanged;
+    protected void BaseBuddyStatusChanged(int buddyId, int status, string text)
+    {
+      if (null != BuddyStatusChanged) BuddyStatusChanged(buddyId, status, text);
+    }
+    public event DDtmfDigitReceived DtmfDigitReceived;
+    protected void BaseDtmfDigitReceived(int callId, int digit)
+    {
+      if (null != DtmfDigitReceived) DtmfDigitReceived(callId, digit);
+    }
+    public event DMessageWaitingNotification MessageWaitingIndication;
+    protected void BaseMessageWaitingIndication(int mwi, string text)
+    {
+      if (null != MessageWaitingIndication) MessageWaitingIndication(mwi, text);
+    }
+
+    #endregion
+
+    private bool _initialized = false;
+    public bool IsInitialized
+    {
+      get { return _initialized; }
+      protected set { _initialized = value; }
+    }
+
+    public abstract int initialize();
+    public abstract int shutdown();
+
+    public abstract int registerAccounts();
+    public abstract int registerAccounts(bool renew);
+
+    public abstract int addBuddy(string ident);
+
+    public abstract int delBuddy(int buddyId);
+
+    public abstract int sendMessage(string dest, string message);
+
+    public abstract int setStatus(int accId, EUserStatus presence_state);
+
+    public abstract void setCodecPrioroty(string item, int p);
+
+    public abstract int getNoOfCodecs();
+
+    public abstract string getCodec(int i);
   }
 
   public interface IMediaProxyInterface
@@ -211,7 +307,7 @@ namespace Telephony
 
   public interface ICallLogInterface
   {
-    // Telephony interface
+    // CallControl interface
     void addCall(ECallType type, string number, string name, System.DateTime time, System.TimeSpan duration);
 
     // GUI interface
@@ -273,4 +369,344 @@ namespace Telephony
     public abstract void Save();
     #endregion Methods
   }
-} // namespace Sipek
+
+  #region Null Implementations
+
+  public class NullTimer : ITimer
+  {
+    #region ITimer Members
+    public override void Start() { }
+    public override void Stop() { }
+    public override int Interval
+    {
+      get { return 100; }
+      set { }
+    }
+
+    public override TimerExpiredCallback Elapsed
+    {
+      set { }
+    }
+    #endregion
+  }
+
+  public class NullConfigurator : IConfiguratorInterface
+  {
+    public class NullAccount : IAccount
+    {
+      public override string AccountName
+      {
+        get { return ""; }
+        set { }
+      }
+
+      public override string HostName
+      {
+        get { return ""; }
+        set { }
+      }
+
+      public override string Id
+      {
+        get { return ""; }
+        set {}
+      }
+
+      public override string UserName
+      {
+        get { return ""; }
+        set {}
+      }
+
+      public override string Password
+      {
+        get { return ""; }
+        set {}
+      }
+
+      public override string DisplayName
+      {
+        get { return ""; }
+        set {}
+      }
+
+      public override string DomainName
+      {
+        get { return""; }
+        set {}
+      }
+
+      public override int Port
+      {
+        get { return 0; }
+        set {}
+      }
+
+      public override int RegState
+      {
+        get { return 0; }
+        set {}
+      }
+    }
+
+    #region IConfiguratorInterface Members
+
+    public override bool CFUFlag
+    {
+      get { return false; }
+      set { }
+    }
+
+    public override string CFUNumber
+    {
+      get { return ""; }
+      set { }
+    }
+
+    public override bool CFNRFlag
+    {
+      get { return false; }
+      set { }
+    }
+
+    public override string CFNRNumber
+    {
+      get { return ""; }
+      set { }
+    }
+
+    public override bool CFBFlag
+    {
+      get { return false; }
+      set { }
+    }
+
+    public override string CFBNumber
+    {
+      get { return ""; }
+      set { }
+    }
+
+    public override bool DNDFlag
+    {
+      get { return false; }
+      set { }
+    }
+
+    public override bool AAFlag
+    {
+      get { return false; }
+      set { }
+    }
+    public override int SIPPort
+    {
+      get { return 5060; }
+      set { }
+    }
+    public override int DefaultAccountIndex
+    {
+      get { return 0; }
+      set { }
+    }
+
+    public override int NumOfAccounts
+    {
+      get { return 1; }
+      set { }
+    }
+
+    public override IAccount getAccount(int index)
+    {
+      return new NullAccount();
+    }
+
+    public override void Save()
+    { }
+
+    public override List<string> CodecList { get { return null; } set { } }
+
+    #endregion
+
+  }
+
+  public class NullCallProxy : ICallProxyInterface
+  {
+    #region ICallProxyInterface Members
+
+    public int makeCall(string dialedNo, int accountId)
+    {
+      return 1;
+    }
+
+    public bool endCall(int sessionId)
+    {
+      return false;
+    }
+
+    public bool alerted(int sessionId)
+    {
+      return false;
+    }
+
+    public bool acceptCall(int sessionId)
+    {
+      return false;
+    }
+
+    public bool holdCall(int sessionId)
+    {
+      return false;
+    }
+
+    public bool retrieveCall(int sessionId)
+    {
+      return false;
+    }
+
+    public bool xferCall(int sessionId, string number)
+    {
+      return false;
+    }
+
+    public bool xferCallSession(int sessionId, int session)
+    {
+      return false;
+    }
+
+    public bool threePtyCall(int sessionId, int session)
+    {
+      return false;
+    }
+
+    public bool serviceRequest(int sessionId, int code, string dest)
+    {
+      return false;
+    }
+
+    public bool dialDtmf(int sessionId, string digits, int mode)
+    {
+      return false;
+    }
+
+    #endregion
+  }
+
+  public class NullCommonProxy : ICommonProxyInterface
+  {
+    #region ICommonProxyInterface Members
+
+    public override int initialize()
+    {
+      return 1;
+    }
+
+    public override int shutdown()
+    {
+      return 1;
+    }
+
+    public override int registerAccounts()
+    {
+      return 1;
+    }
+
+    public override int registerAccounts(bool renew)
+    {
+      return 1;
+    }
+
+    public override int addBuddy(string ident)
+    {
+      return 1;
+    }
+
+    public override int delBuddy(int buddyId)
+    {
+      return 1;
+    }
+
+    public override int sendMessage(string dest, string message)
+    {
+      return 1;
+    }
+
+    public override int setStatus(int accId, EUserStatus presence_state)
+    {
+      return 1;
+    }
+
+    public override void setCodecPrioroty(string item, int p)
+    {
+    }
+    public override int getNoOfCodecs() { return 0; }
+
+    public override string getCodec(int i) { return ""; }
+
+    #endregion
+  }
+
+  public class NullMediaProxy : IMediaProxyInterface
+  {
+    #region IMediaProxyInterface Members
+
+    public int playTone(ETones toneId)
+    {
+      return 1;
+    }
+
+    public int stopTone()
+    {
+      return 1;
+    }
+    #endregion
+  }
+
+  /// <summary>
+  /// Null Factory implementation
+  /// </summary>
+  public class NullFactory : AbstractFactory
+  {
+    IConfiguratorInterface _config = new NullConfigurator();
+    ICommonProxyInterface _common = new NullCommonProxy();
+    IMediaProxyInterface _media = new NullMediaProxy();
+    ICallLogInterface _logger = new CNullCallLog();
+
+    #region AbstractFactory members
+    // factory methods
+    public ITimer createTimer()
+    {
+      return new NullTimer();
+    }
+
+    //TODO
+    public ICallProxyInterface createCallProxy()
+    {
+      return new NullCallProxy();
+    }
+
+    public ICommonProxyInterface getCommonProxy()
+    {
+      return _common;
+    }
+
+    public IConfiguratorInterface getConfigurator()
+    {
+      return _config;
+    }
+
+    // Implement getters
+    public IMediaProxyInterface getMediaProxy()
+    {
+      return _media;
+    }
+
+    public ICallLogInterface getCallLogger()
+    {
+      return _logger;
+    }
+    #endregion
+  }
+  #endregion
+
+
+} // namespace Common
